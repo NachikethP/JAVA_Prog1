348fdcd25ba207a76dfeeefde03985289e4e3823
NachikethP <68091846+NachikethP@users.noreply.github.com>
2020-08-08 18:10:31 +0530

    print hello world

---
+++ hello_worl.java
@@ -0,0 +1,11 @@
+public class sort {
+
+    public static void sopln(String str){
+            System.out.println(str);
+
+        }
+    public static void main(String args[]){
+            sort obj=new sort();
+            obj.sopln("HELLO WORLD");
+    }
+}
